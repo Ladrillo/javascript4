2d085a9261339dbb9894f7b5c3da0e0b0517de30
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-22 09:36:52 +0200

    initial

---
+++ readme.md
@@ -0,0 +1,5 @@
+# JavaScript 4
+
+- Clone repo.
+- Bring in the project's dependencies by running `npm install`.
+- Launch the development server by running `npm start`.
---
+++ script.js
@@ -0,0 +1,11 @@
+var greeting = 'hello from script';
+
+// A- SEUDO-CLASSIC INHERITANCE & PROTOTYPAL INHERITANCE
+//    1- Create a Vehicle constructor that "subclasses" Object.
+//    2- Create a Car constructor that "subclasses" Vehicle.
+//    3- Create a Tesla constructor that "subclasses" Car.
+//    4- Create a Truck constructor that "subclasses" Vehicle.
+//    5- Inspect the __proto__ prop on your vehicle instances. See the chain?
+
+// B- CLASS SYNTACTIC SUGAR
+//    10- Refactor to use the new `class` and `extends` keywords
