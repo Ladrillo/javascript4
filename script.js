85f778e67190955177e6d62b13e1f719f520b482
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-22 10:30:24 +0200

    wip

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 var greeting = 'hello from script';
 
+function Bulb(model) {
+  this.model = model;
+  this.isOn = false;
+}
+Bulb.prototype.turnOn = function () {
+  // side effect
+  this.isOn = true;
+  // return value
+  return 'turnin on!'
+}
+
+var myBulb = new Bulb('Philips');
+// myBulb gets assigned a __proto__ property holding
+// the methods!! wohooo
+
 // A- SEUDO-CLASSIC INHERITANCE & PROTOTYPAL INHERITANCE
 //    1- Create a Vehicle constructor that "subclasses" Object.
 //    2- Create a Car constructor that "subclasses" Vehicle.
